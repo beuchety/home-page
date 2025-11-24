d47f13919d8df4fc705327d1fc7b6ddeb49ccb3b
beuchety <beuchety@gmail.com>
2025-11-23 20:42:29 -0700

    inital commit

---
+++ data.js
@@ -0,0 +1,37 @@
+var data = [
+    {
+        "name": "Synchrony",
+        "url": "https://id.synchrony.com/idp/en",
+        "due": 29
+    },
+    {
+        "name": "Home Depot",
+        "url": "https://www.homedepot.com/myaccount/thdcreditcard",
+        "due": 30
+    },
+    {
+        "name": "Verizon",
+        "url": "https://secure.verizon.com/signin",
+        "due": 3
+    },
+    {
+        "name": "BarclayUS",
+        "url": "https://cards.barclaycardus.com/",
+        "due": 5
+    },
+    {
+        "name": "Tithing",
+        "url": "https://www.churchofjesuschrist.org/?lang=eng",
+    },
+    {
+        "name": "b1",
+        "url": "https://cards.barclaycardus.com/",
+        "due": 22
+    },
+    {
+        "name": "b1",
+        "url": "https://cards.barclaycardus.com/",
+        "due": 23
+    },
+
+];
